1e6a8dd1de3eca7ca650f38eaf2c5f54f149307d
Akshat091296 <akshat091296@gmail.com>
2016-12-27 18:13:06 +0530

    Update educash_allotment_and_history.php

--- educash_allotment_and_history.php
+++ educash_allotment_and_history.php
@@ -12,6 +12,10 @@ function educash_deals_form_page()
             $clientnamerr = "<span  style='color:red;'>This field cannot be blank</span>";
         } else {
             $clientName = $_POST['clientName'];
+            $check_client = $wpdp->get_results("SELECT ID from $wpdb->prefix.users WHERE ID = '$clientName' ");
+            if($check_client == NULL){
+                $invalid_client = "<span style='color:red'>This client does not exist in our database</span>";
+            }
         }
 
         if (empty($_POST['educash'])) {
@@ -20,7 +24,7 @@ function educash_deals_form_page()
             $educash = $_POST['educash'];
         }
 
-        if ((!empty($_POST['clientName'])) && (!empty($_POST['educash']))) {
+        if ((!empty($_POST['clientName'])) && (!empty($_POST['educash'])) && (!($ckeck_client == NULL))) {
             $adminName = wp_get_current_user();
             $adminComment = $_POST['adminComment'];
             $time = current_time('mysql');
@@ -44,7 +48,7 @@ function educash_deals_form_page()
 ?>
     <div style='display:inline-block; width:48%;'><h2>Use this form to allocate educash to a client</h2><br/>
     <form method='post' onsubmit="return confirm('Do you really want to submit this entry?');" action="<?php echo $_SERVER['REQUEST_URI'];?>">
-             Client Name (Type the name of the client whom you want to allot educash):<br/><input type='text' name='clientName' maxlength='70'><span>* <?php echo $clientnamerr;?> </span><br/><br/>
+             Client ID (Type the ID of the client whom you want to allot educash):<br/><input type='number' name='clientName' min='1' max='1000000000000'><span>* <?php echo $clientnamerr; echo $invalid_client;?> </span><br/><br/>
              Type the educash to be added in the client's account:<br/><input type='number' name='educash' min='-100000000' max='100000000'><span>* <?php echo $educasherr;?> </span><br/><br/>
              Type your comments here (optional):<br/><textarea rows='4' cols='60' name='adminComment' maxlength='500'></textarea><br/><br/>
              <input type='submit' name='submit'><br/>
@@ -53,8 +57,8 @@ function educash_deals_form_page()
     echo "<div style='display:inline-block; width:48%;'><h2>Use this form to know the history of educash transactions</h2>";
     echo "<p>Fill atleast one field<p>";
     echo "<form method='post' action='" . $_SERVER['REQUEST_URI'] . "'>
-             Admin ID (Type the ID of the admin whose history you want to see):<br/><input type='number' name='admin_Name' min='1' max='100000000'><br/><br/>
-             Client Name (Type the name of the client whose history you want to see):<br/><input type='text' name='client_Name' maxlength='70'><br/><br/>
+             Admin ID (Type the ID of the admin whose history you want to see):<br/><input type='number' name='admin_Name' min='1' max='1000000000000'><br/><br/>
+             Client ID (Type the ID of the client whose history you want to see):<br/><input type='number' name='client_Name' min='1' max='1000000000000'><br/><br/>
              Date (Select the date whose transaction details you want to see):<br/><input type='date' name='date' min='1990-12-31' max='2050-12-31'><br/><br/>
              <input type='submit' name='Submit'>" . $all_three_error . "<br/><br/><br/>
              </form></div>";
